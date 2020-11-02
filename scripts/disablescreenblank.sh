76820ea0a14d395ca5e166e647cc8354bdd7d763
Jordan Ruthe <jordan.ruthe@gmail.com>
2020-11-02 12:04:45 -0500

    Adding disable screen blank to startup script

--- scripts/disablescreenblank.sh
+++ scripts/disablescreenblank.sh
@@ -2,10 +2,9 @@
 
 screen=${1:-0}
 
-# wait 10s for the display manager service to start and attach to screen
+# wait for the display manager service to start and attach to screen
 sleep 5
 
 /usr/bin/xset -display :$screen s off          # deactivate screen saver
 /usr/bin/xset -display :$screen -dpms          # disable DPMS
 /usr/bin/xset -display :$screen s noblank      # disable screen blanking
-
