369fc24e16c53a6ff4a9bd65f7e81bee2958e45c
Jordan Ruthe <jordan.ruthe@gmail.com>
2020-12-03 13:40:25 -0500

    screen: include "error" for state calculation

--- screen.py
+++ screen.py
@@ -408,7 +408,7 @@ class KlipperScreen(Gtk.Window):
         else:
             self.files.add_timeout()
 
-        if printer_info['result']['state'] == "shutdown":
+        if printer_info['result']['state'] in ("error","shutdown"):
             if "FIRMWARE_RESTART" in printer_info['result']['state_message']:
                 self.printer_initializing(
                     _("Klipper has encountered an error.\nIssue a FIRMWARE_RESTART to attempt fixing the issue.")
