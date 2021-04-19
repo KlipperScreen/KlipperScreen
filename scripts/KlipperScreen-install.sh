0e1c30879bd9660a33ab4b9ed281829aa8c5d422
Jordan Ruthe <jordan.ruthe@gmail.com>
2021-04-18 23:42:04 -0400

    install: Change install command to PKGLIST This enables moonraker update compatability for installing system packages

--- scripts/KlipperScreen-install.sh
+++ scripts/KlipperScreen-install.sh
@@ -4,27 +4,17 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 KSPATH=$(sed 's/\/scripts//g' <<< $SCRIPTPATH)
 KSENV="${HOME}/.KlipperScreen-env"
 
+PKGLIST="xserver-xorg-video-fbturbo xdotool xinit xinput x11-xserver-utils python3-distutils python3-gi"
+PKGLIST="${PKGLIST} python3-gi-cairo python3-virtualenv gir1.2-gtk-3.0 virtualenv matchbox-keyboard wireless-tools"
+PKGLIST="${PKGLIST} libatlas-base-dev"
+
 install_packages()
 {
     echo "Update package data"
     sudo apt update
 
     echo "Installing package dependencies"
-    sudo apt install -y \
-        xserver-xorg-video-fbturbo \
-        xdotool \
-        xinit \
-        xinput \
-        x11-xserver-utils \
-        python3-distutils \
-        python3-gi \
-        python3-gi-cairo \
-        python3-virtualenv \
-        gir1.2-gtk-3.0 \
-        virtualenv \
-        matchbox-keyboard \
-        wireless-tools \
-        libatlas-base-dev
+    sudo apt install -y $PKGLIST
 }
 
 create_virtualenv()
