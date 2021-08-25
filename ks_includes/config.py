17f7d9280f4f2dc12e600f58ea92423d7a243b5c
alfrix <alfredomonclus@gmail.com>
2021-08-25 00:08:21 -0300

    Macro shortcut, ask to reload

--- ks_includes/config.py
+++ ks_includes/config.py
@@ -132,7 +132,7 @@ class KlipperScreenConfig:
                     {"name": _("Colorized"), "value": "colorized"}
             ]}},
             {"24htime": {"section": "main", "name": _("24 Hour Time"), "type": "binary", "value": "True"}},
-            {"side_macro_shortcut": {"section": "main", "name": _("Macro shortcut on sidebar"), "type": "binary", "value": "True"}},
+            {"side_macro_shortcut": {"section": "main", "name": _("Macro shortcut on sidebar"), "type": "binary", "value": "True", "callback": screen.restart_warning}},
             #{"": {"section": "main", "name": _(""), "type": ""}}
         ]
 
