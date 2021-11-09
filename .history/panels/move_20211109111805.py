f97404ec0a0c0126a88d98c360da028a4c37e275
Transistor427 <47721986+Transistor427@users.noreply.github.com>
2021-11-09 11:19:15 +0300

    Смена местами Z+ и Z-

--- .history/panels/move_20211109111805.py
+++ .history/panels/move_20211109111805.py
@@ -47,8 +47,8 @@ class MovePanel(ScreenPanel):
             grid.attach(self.labels['x-'], 2, 1, 1, 1)
         grid.attach(self.labels['y+'], 1, 0, 1, 1)
         grid.attach(self.labels['y-'], 1, 1, 1, 1)
-        grid.attach(self.labels['z+'], 3, 1, 1, 1)
-        grid.attach(self.labels['z-'], 3, 0, 1, 1)
+        grid.attach(self.labels['z+'], 3, 0, 1, 1)
+        grid.attach(self.labels['z-'], 3, 1, 1, 1)
 
         grid.attach(self.labels['home'], 0, 0, 1, 1)
 
