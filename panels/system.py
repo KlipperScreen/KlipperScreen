b52085746fd978065f048de68262ee836f269b75
Elektrickser <37211274+Elektrickser@users.noreply.github.com>
2021-11-17 03:03:46 +0100

    More German Translations (#318)
    * Add German Language
    
    Translate the texts to german.
    
    * Update system.py
    
    change grid, to see all buttons on screen
    
    * Revert "Add German Language"
    
    This reverts commit 698225883947d4237559009d82f9f34885fb0a42.
    
    * Add German Language
    
    add German translated files.
    
    * fixed german translation
    
    fixed "Laden"
    
    * fixed german translation
    
    add more translations
    
    * Update system.py

--- panels/system.py
+++ panels/system.py
@@ -103,7 +103,6 @@ class SystemPanel(ScreenPanel):
         grid.attach(firmrestart, 1, 2, 1, 1)
         grid.attach(reboot, 2, 2, 1, 1)
         grid.attach(shutdown, 3, 2, 1, 1)
-
         self.content.add(grid)
 
     def activate(self):
