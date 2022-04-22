6f9da6072f9c147d281788830e13d1af611a9b5d
DmitriyAD <alexandrovi4dd@gmail.com>
2022-04-22 14:19:08 +0300

    d

--- panels/extrude.py
+++ panels/extrude.py
@@ -43,7 +43,7 @@ class ExtrudePanel(ScreenPanel):
         self.labels['unload'].connect("clicked", self.load_unload, "-")
         self.labels['retract'] = self._gtk.ButtonImage("retract", _("Втянуть"), "color1")
         self.labels['retract'].connect("clicked", self.extrude, "-")
-        self.labels['nozzle'] = self._gtk.ButtonImage("fan", _("Очистка"), "color3")
+        self.labels['nozzle'] = self._gtk.ButtonImage("clean-nozzle", _("Очистка"), "color3")
         self.labels['nozzle'].connect("clicked", self.clean_nozzle)
         self.labels['temperature'] = self._gtk.ButtonImage("heat-up", _("Temperature"), "color4")
         self.labels['temperature'].connect("clicked", self.menu_item_clicked, "temperature", {
