7f4970b16dfbb2cbd00ae73c447a8d312ac30c00
alfrix <alfredomonclus@gmail.com>
2022-05-09 17:32:50 -0300

    macros: allow reverse sorting

--- panels/gcode_macros.py
+++ panels/gcode_macros.py
@@ -2,7 +2,7 @@ import gi
 import logging
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gdk, Gtk, Pango
+from gi.repository import Gdk, Gtk, GLib, Pango
 
 from ks_includes.screen_panel import ScreenPanel
 
@@ -14,6 +14,8 @@ class MacroPanel(ScreenPanel):
         _ = self.lang.gettext
         self.macros = {}
         self.loaded_macros = []
+        self.sort_char = [" ↑", " ↓"]
+        self.sort_reverse = False
 
         # Create a scroll window for the macros
         scroll = Gtk.ScrolledWindow()
@@ -26,23 +28,35 @@ class MacroPanel(ScreenPanel):
         self.labels['macros'] = Gtk.Grid()
         scroll.add(self.labels['macros'])
 
+        sort = Gtk.Label(_("Sort:"))
+        sort.set_hexpand(False)
+        self.sort_lbl = _("Name")
+        self.sort_btn = self._gtk.Button(self.sort_lbl + self.sort_char[0], "color1")
+        self.sort_btn.connect("clicked", self.change_sort)
+        self.sort_btn.set_hexpand(True)
+        #adjust = self._gtk.ButtonImage('fine-tune', '', "color2", 1, Gtk.PositionType.LEFT, False)
+
+        sbox = Gtk.HBox()
+        sbox.set_vexpand(False)
+        sbox.pack_start(sort, False, False, 5)
+        sbox.pack_start(self.sort_btn, True, True, 5)
+        #sbox.pack_start(adjust, True, True, 5)
+
         # Create a box to contain all of the above
-        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        box = Gtk.VBox()
         box.set_vexpand(True)
+        box.pack_start(sbox, False, False, 0)
         box.pack_start(scroll, True, True, 0)
 
         self.content.add(box)
 
     def activate(self):
-        self.unload_gcode_macros()
-        self.load_gcode_macros()
+        self.reload_macros()
 
     def add_gcode_macro(self, macro):
         # Support for hiding macros by name
         if macro.startswith("_"):
             return
-        frame = Gtk.Frame()
-        frame.get_style_context().add_class("frame-item")
 
         name = Gtk.Label()
         name.set_markup("<big><b>%s</b></big>" % (macro))
@@ -53,33 +67,32 @@ class MacroPanel(ScreenPanel):
         name.set_line_wrap(True)
         name.set_line_wrap_mode(Pango.WrapMode.WORD_CHAR)
 
-        open = self._gtk.ButtonImage("resume", None, "color3")
-        open.connect("clicked", self.run_gcode_macro, macro)
-        open.set_hexpand(False)
-        open.set_halign(Gtk.Align.END)
+        btn = self._gtk.ButtonImage("resume", None, "color3")
+        btn.connect("clicked", self.run_gcode_macro, macro)
+        btn.set_hexpand(False)
+        btn.set_halign(Gtk.Align.END)
 
-        labels = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        labels = Gtk.VBox()
         labels.add(name)
 
-        dev = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
-        dev.set_hexpand(True)
-        dev.set_vexpand(False)
-
+        dev = Gtk.HBox(spacing=5)
         dev.add(labels)
-        dev.add(open)
+        dev.add(btn)
+
+        frame = Gtk.Frame()
+        frame.get_style_context().add_class("frame-item")
         frame.add(dev)
 
         self.macros[macro] = {
             "row": frame
         }
 
-        macros = sorted(self.macros)
+        macros = sorted(self.macros, reverse=self.sort_reverse, key=str.casefold)
         pos = macros.index(macro)
 
         self.loaded_macros.append(macro)
         self.labels['macros'].insert_row(pos)
         self.labels['macros'].attach(self.macros[macro]['row'], 0, pos, 1, 1)
-        self.labels['macros'].show_all()
 
     def load_gcode_macros(self):
         macros = self._screen.printer.get_gcode_macros()
@@ -95,16 +108,22 @@ class MacroPanel(ScreenPanel):
             if (section_name not in self._config.get_config().sections() or
                     self._config.get_config().getboolean(section_name, macro.lower(), fallback=True)):
                 self.add_gcode_macro(macro)
+        self.labels['macros'].show_all()
 
     def run_gcode_macro(self, widget, macro):
         self._screen._ws.klippy.gcode_script(macro)
 
-    def unload_gcode_macros(self):
-        section_name = "displayed_macros %s" % self._screen.connected_printer
-        for macro in self.loaded_macros:
-            if (section_name in self._config.get_config().sections() and
-                    not self._config.get_config().getboolean(section_name, macro.lower(), fallback=True)):
-                macros = sorted(self.macros)
-                pos = macros.index(macro)
-                self.labels['macros'].remove_row(pos)
-                self.labels['macros'].show_all()
+    def change_sort(self, widget):
+        self.sort_reverse ^= True
+        if self.sort_reverse:
+            self.sort_btn.set_label(self.sort_lbl + self.sort_char[1])
+        else:
+            self.sort_btn.set_label(self.sort_lbl + self.sort_char[0])
+        self.sort_btn.show()
+
+        GLib.idle_add(self.reload_macros)
+
+    def reload_macros(self):
+        self.labels['macros'].remove_column(0)
+        self.loaded_macros = []
+        self.load_gcode_macros()
