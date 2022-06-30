22fde167c5621201eeaed71f5c3877657c3c004d
alfrix <alfredomonclus@gmail.com>
2022-06-30 11:17:01 -0300

    Optimize imports

--- panels/bed_mesh.py
+++ panels/bed_mesh.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gdk, Gtk, Pango
+from gi.repository import Gtk, Pango
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
--- panels/system.py
+++ panels/system.py
@@ -1,6 +1,5 @@
 import gi
 import logging
-import os
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gdk, Gtk, Pango
