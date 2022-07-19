67b7429dc1caa73c3e20b03d9d3b81912545b854
Franklyn Tackitt <git@frank.af>
2022-07-19 09:37:17 -0700

    Make installation configurable
    ```sh
    KLIPPERSCREEN_VENV=$HOME/.venvs/klipperscreen ./scripts/KlipperScreen-install.sh
    ```

--- scripts/KlipperScreen-install.sh
+++ scripts/KlipperScreen-install.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 KSPATH=$(sed 's/\/scripts//g' <<< $SCRIPTPATH)
-KSENV="${HOME}/.KlipperScreen-env"
+KSENV="${KLIPPERSCREEN_VENV:-${HOME}/.KlipperScreen-env}"
 
 XSERVER="xinit xinput x11-xserver-utils xdotool"
 FBTURBO="xserver-xorg-video-fbturbo"
