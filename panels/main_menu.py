c13536907a3ef1e546910baf43c6c0106d0af5c7
alfrix <alfredomonclus@gmail.com>
2022-09-08 14:45:42 -0300

    main and temp: revert removal of the header

--- panels/main_menu.py
+++ panels/main_menu.py
@@ -201,6 +201,13 @@ class MainPanel(MenuPanel):
         self.labels['devices'].get_style_context().add_class('heater-grid')
         self.labels['devices'].set_vexpand(False)
 
+        name = Gtk.Label("")
+        temp = Gtk.Label(_("Temp (°C)"))
+        temp.set_size_request(round(self._gtk.get_font_size() * 7.7), -1)
+
+        self.labels['devices'].attach(name, 0, 0, 1, 1)
+        self.labels['devices'].attach(temp, 1, 0, 1, 1)
+
         self.labels['da'] = HeaterGraph(self._printer, self._gtk.get_font_size())
         self.labels['da'].set_vexpand(True)
 
