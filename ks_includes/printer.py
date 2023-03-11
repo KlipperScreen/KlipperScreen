KlipperScreen/KlipperScreen

commit dfe1ac8d2ce6ac0ce799e8c1e77262372de85b3a

macros: fix list not loading when there are extra spaces fixes #909

ks_includes/printer.py
@@ -153,7 +153,14 @@ class Printer:
         return []
 
     def get_config_section(self, section):
-        return self.config[section] if section in self.config else False
+        return next(
+            (
+                self.config[key]
+                for key in self.config.keys()
+                if key.find(section) > -1
+            ),
+            False,
+        )
 
     def get_fans(self):
         fans = []

panels/gcode_macros.py
@@ -83,7 +83,12 @@ class MacroPanel(ScreenPanel):
             "params": {},
         }
         pattern = r'params\.(?P<param>..*)\|default\((?P<default>..*)\).*'
-        gcode = self._printer.get_config_section(f"gcode_macro {macro}")["gcode"].split("\n")
+        gcode = self._printer.get_config_section(macro)
+        if gcode and "gcode" in gcode:
+            gcode = gcode["gcode"].split("\n")
+        else:
+            logging.debug(f"Couldn't load {macro}\n{gcode}")
+            return
         i = 0
         for line in gcode:
             if line.startswith("{") and "params." in line:

commit 87c3f03719437781aabd7bdb6b0d5064c529f14b

fix issues caused by dfe1ac8d
fix #924
fix #923

ks_includes/printer.py
@@ -153,11 +153,14 @@ class Printer:
         return []
 
     def get_config_section(self, section):
+        return self.config[section] if section in self.config else False
+
+    def get_macro(self, macro):
         return next(
             (
                 self.config[key]
                 for key in self.config.keys()
-                if key.find(section) > -1
+                if key.find(macro) > -1
             ),
             False,
         )