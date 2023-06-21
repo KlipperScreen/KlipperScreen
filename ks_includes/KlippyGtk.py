a973300d2fc10cd58a7eb0bc1eb3c222bf659ebe
alfrix <alfredomonclus@gmail.com>
2023-06-21 17:30:25 -0300

    klippygtk: scaling tweak for default text size to fit 4 sliders without scrollbars (limits and retraction)

--- ks_includes/KlippyGtk.py
+++ ks_includes/KlippyGtk.py
@@ -164,7 +164,7 @@ class KlippyGtk:
             if scale is None:
                 scale = self.button_image_scale
             if label is None:
-                scale = scale * 1.5
+                scale = scale * 1.4
             width = height = self.img_scale * scale
             b.set_image(self.Image(image_name, width, height))
         b.set_image_position(position)
