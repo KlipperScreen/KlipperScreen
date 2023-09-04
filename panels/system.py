001c1cb009839f85fe7d6a2bdf227a4cad40c0ea
alfrix <alfredomonclus@gmail.com>
2023-09-03 22:11:28 -0300

    rest: add timeout parameter for system update refresh

--- panels/system.py
+++ panels/system.py
@@ -97,7 +97,7 @@ class Panel(ScreenPanel):
         GLib.timeout_add_seconds(1, self.get_updates, "true")
 
     def get_updates(self, refresh="false"):
-        update_resp = self._screen.apiclient.send_request(f"machine/update/status?refresh={refresh}")
+        update_resp = self._screen.apiclient.send_request(f"machine/update/status?refresh={refresh}", timeout=60)
         if not update_resp:
             self.update_status = {}
             logging.info("No update manager configured")
