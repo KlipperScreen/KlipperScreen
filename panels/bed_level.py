fe664b44a5e5e066f1d908ad35f902d0f36131d5
alfrix <alfredomonclus@gmail.com>
2023-10-08 12:02:29 -0300

    bed_level: hotfix for b6199a4

--- panels/bed_level.py
+++ panels/bed_level.py
@@ -80,6 +80,9 @@ class Panel(ScreenPanel):
 
             self.screws = new_screws
             logging.info(f"screws with offset: {self.screws}")
+        elif "bed_screws" in self._printer.get_config_section_list():
+            self.screws = self._get_screws("bed_screws")
+            logging.info(f"bed_screws: {self.screws}")
 
         nscrews = len(self.screws)
         # KS config
@@ -107,9 +110,6 @@ class Panel(ScreenPanel):
             if nscrews in (3, 5, 7):
                 valid_positions = False
             screw_positions = valid_screws
-        if 'bed_screws' in self._config.get_config():
-            rotation = self._config.get_config()['bed_screws'].getint("rotation", 0)
-            logging.debug(f"Rotation: {rotation}")
 
         # get dimensions
         x_positions = {x[0] for x in self.screws}
