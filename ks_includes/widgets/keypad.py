f5a5efabda546fd2216d74d5beeffa73d1577a6c
alfrix <alfredomonclus@gmail.com>
2023-10-15 14:46:36 -0300

    styles: use gtk vars for colors and other cleanup and improvements close #594

--- ks_includes/widgets/keypad.py
+++ ks_includes/widgets/keypad.py
@@ -42,6 +42,7 @@ class Keypad(Gtk.Box):
                 self.labels[k_id] = Gtk.Button(label=keys[i][0])
             self.labels[k_id].connect('clicked', self.update_entry, keys[i][0])
             self.labels[k_id].get_style_context().add_class(keys[i][1])
+            self.labels[k_id].get_style_context().add_class("numpad_key")
             numpad.attach(self.labels[k_id], i % 3, i / 3, 1, 1)
 
         self.labels["keypad"] = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
