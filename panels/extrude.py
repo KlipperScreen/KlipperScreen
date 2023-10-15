02799f2386b7b1c8ccd6a68db2c10e9e60252bf4
Anton Chebotaev <anton.chebotaev@gmail.com>
2023-10-15 20:01:15 +0300

    Allow to use T0 even with 1 extruder
    I think it still a useful button, which can be used with a redefined T0 macro, which could be utilized with cooling down a hotend for example

--- panels/extrude.py
+++ panels/extrude.py
@@ -58,10 +58,10 @@ class Panel(ScreenPanel):
         i = 0
         for extruder in self._printer.get_tools():
             if self._printer.extrudercount > 1:
-                self.labels[extruder] = self._gtk.Button(f"extruder-{i}", f"T{self._printer.get_tool_number(extruder)}")
-                self.labels[extruder].connect("clicked", self.change_extruder, extruder)
+                self.labels[extruder] = self._gtk.Button(f"extruder-{i}", f"T{self._printer.get_tool_number(extruder)}")                
             else:
                 self.labels[extruder] = self._gtk.Button("extruder", "")
+            self.labels[extruder].connect("clicked", self.change_extruder, extruder)
             if extruder == self.current_extruder:
                 self.labels[extruder].get_style_context().add_class("button_active")
             if i < limit:
