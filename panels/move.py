38f37198c7a4a0b884d512690336afd8a52d15e6
cdsmith <csmith@lulzbot.com>
2024-01-17 13:43:34 -0600

    Fix move distance button defaults.

--- panels/move.py
+++ panels/move.py
@@ -16,7 +16,7 @@ class Panel(ScreenPanel):
         super().__init__(screen, title)
 
         if self.ks_printer_cfg is not None:
-            dis = self.ks_printer_cfg.get("move_distances", '0.1, 0.5, 1, 50, 10, 25, 50')
+            dis = self.ks_printer_cfg.get("move_distances", '0.1, 0.5, 1, 5, 10, 25, 50')
             if re.match(r'^[0-9,\.\s]+$', dis):
                 dis = [str(i.strip()) for i in dis.split(',')]
                 if 1 < len(dis) <= 7:
