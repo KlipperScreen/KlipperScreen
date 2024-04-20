c75ba990c810624b174a35d274e7f36ea2c9490a
alfrix <alfredomonclus@gmail.com>
2024-04-19 21:32:19 -0300

    macros: send gcode style (G/M) macros without =

--- panels/gcode_macros.py
+++ panels/gcode_macros.py
@@ -101,7 +101,10 @@ class Panel(ScreenPanel):
         for param in self.macros[macro]["params"]:
             value = self.macros[macro]["params"][param].get_text()
             if value:
-                params += f' {param}={value}'
+                if re.findall(r'[G|M]\d{1,3}', macro):
+                    params += f' {param}{value}'
+                else:
+                    params += f' {param}={value}'
         self._screen.show_popup_message(f"{macro} {params}", 1)
         self._screen._send_action(widget, "printer.gcode.script", {"script": f"{macro}{params}"})
 
