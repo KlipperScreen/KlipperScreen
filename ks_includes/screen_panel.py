dc2165fee45c38871850421c031253149c6515a9
ZoZo <cor@ruiten.com>
2024-04-26 18:35:20 -0700

    Screen Panel: Fixed emergency stop confirmation process (#1346)
    Emergency stop was always proceeding without waiting for confirmation, despite the fact that user configured KlipperScreen to confirm emergency stops.
    fixes #1344

--- ks_includes/screen_panel.py
+++ ks_includes/screen_panel.py
@@ -43,7 +43,6 @@ class ScreenPanel:
                                               "printer.emergency_stop")
         else:
             self._screen._ws.klippy.emergency_stop()
-        self._screen._ws.klippy.emergency_stop()
 
     def get_file_image(self, filename, width=None, height=None, small=False):
         if not self._files.has_thumbnail(filename):
