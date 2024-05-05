32d0af4679cc02a999cf7242bb5a50ee77825ef4
alfrix <alfredomonclus@gmail.com>
2024-05-05 09:31:20 -0300

    fix screen is blank after update under certain configs

--- ks_includes/screen_panel.py
+++ ks_includes/screen_panel.py
@@ -172,14 +172,14 @@ class ScreenPanel:
         if dev in self.devices and not self.devices[dev]["can_target"]:
             show_target = False
 
-        show_power = show_target and self._show_heater_power and power is not None
+        show_power = show_target and self._show_heater_power and power
 
         new_label_text = f"{temp:.0f}"
         if show_target:
             new_label_text += f"/{target:.0f}"
         if dev not in self.devices:
             new_label_text += "°"
-        if show_power and power:
+        if show_power:
             if lines == 2:
                 # The label should wrap, but it doesn't work
                 # this is a workaround
