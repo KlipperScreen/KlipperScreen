629c86d95f669b242265c069f93ffb05dd5163fa
Alfredo Monclus <alfredomonclus@gmail.com>
2024-06-12 21:57:26 -0300

    shutdown: add space to restart

--- panels/shutdown.py
+++ panels/shutdown.py
@@ -21,7 +21,7 @@ class Panel(ScreenPanel):
         restart = self._gtk.Button("refresh", _("Restart"), "color3")
         restart.connect("clicked", self.reboot_poweroff, "reboot")
 
-        restart_ks = self._gtk.Button("refresh", _("Restart") + "KlipperScreen", "color3")
+        restart_ks = self._gtk.Button("refresh", _("Restart") + " KlipperScreen", "color3")
         restart_ks.connect("clicked", self._screen.restart_ks)
 
         self.main = Gtk.Grid(row_homogeneous=True, column_homogeneous=True)
