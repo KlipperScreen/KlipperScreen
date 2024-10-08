ab308e23f743167b53e85090fa0015f33ff27eab
Alfredo Monclus <alfredomonclus@gmail.com>
2024-10-08 12:20:58 -0300

    feat(bed_mesh): do z_tilt or quad_level if not done before

--- panels/bed_mesh.py
+++ panels/bed_mesh.py
@@ -241,6 +241,16 @@ class Panel(ScreenPanel):
         self._screen.show_popup_message(_("Calibrating"), level=1)
         if self._printer.get_stat("toolhead", "homed_axes") != "xyz":
             self._screen._ws.klippy.gcode_script("G28")
+        if (
+            "Z_TILT_ADJUST" in self._printer.available_commands
+            and not bool(self._printer.get_stat("z_tilt", "applied"))
+        ):
+            self._screen._ws.klippy.gcode_script("Z_TILT_ADJUST")
+        if (
+            "QUAD_GANTRY_LEVEL" in self._printer.available_commands
+            and not bool(self._printer.get_stat("quad_gantry_level", "applied"))
+        ):
+            self._screen._ws.klippy.gcode_script("QUAD_GANTRY_LEVEL")
         self._screen._send_action(widget, "printer.gcode.script", {"script": "BED_MESH_CALIBRATE"})
 
     def send_clear_mesh(self, widget):
