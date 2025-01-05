fa1dbe61a2a69100b2fe3397119151ce40cc8465
Andrew Viola <aviola@vt.edu>
2025-01-05 18:48:20 -0500

    Working on touchscreen interface

--- panels/example.py
+++ panels/example.py
@@ -10,6 +10,36 @@ class Panel(ScreenPanel):
         title = title or _("Example")
         super().__init__(screen, title)
 
-        # Create gtk items here
+        # Create a vertical box to hold the buttons
+        button_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        button_box.set_margin_top(20)
+        button_box.set_margin_bottom(20)
+        button_box.set_margin_start(20)
+        button_box.set_margin_end(20)
 
-        self.content.add(Gtk.Box())
+        # Create Button 1
+        button1 = Gtk.Button(label="Button 1")
+        button1.connect("clicked", self.on_button1_clicked)
+        button_box.pack_start(button1, True, True, 0)
+
+        # Create Button 2
+        button2 = Gtk.Button(label="Button 2")
+        button2.connect("clicked", self.on_button2_clicked)
+        button_box.pack_start(button2, True, True, 0)
+
+        # Create Button 3
+        button3 = Gtk.Button(label="Button 3")
+        button3.connect("clicked", self.on_button3_clicked)
+        button_box.pack_start(button3, True, True, 0)
+
+        # Add the button box to the panel
+        self.content.add(button_box)
+
+    def on_button1_clicked(self, widget):
+        print("Button 1 clicked")
+
+    def on_button2_clicked(self, widget):
+        print("Button 2 clicked")
+
+    def on_button3_clicked(self, widget):
+        print("Button 3 clicked")
