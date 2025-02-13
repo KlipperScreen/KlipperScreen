9d1522960810d2809e8d3185bd02e9bafa018c13
Bruno Ottonelli Copatti <ottonellicopattibruno@gmail.com>
2025-02-13 10:04:10 -0300

    Restart ks when finish welcome panel

--- panels/sx_welcome.py
+++ panels/sx_welcome.py
@@ -73,4 +73,4 @@ class Panel(ScreenPanel):
     def finish(self, button):
         self._config.set("syncraft", "welcome", "False")
         self._config.save_user_config_options()
-        self._screen.show_panel("main_menu", remove_all=True, items=self._config.get_menu_items("__main"))
+        self._screen.restart_ks()
