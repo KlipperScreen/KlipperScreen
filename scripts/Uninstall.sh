0a6724e7fbe67b5de7faaadca866d4ee2a3ce742
Alfredo Monclus <alfredomonclus@gmail.com>
2026-05-03 20:19:11 -0300

    fix: improve the uninstall script mainly last message fixes #1681

--- scripts/Uninstall.sh
+++ scripts/Uninstall.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+if [ "$EUID" -eq 0 ]; then
+    echo "Please do not run this script as root"
+    exit 1
+fi
+
 echo "Uninstalling KlipperScreen"
 echo ""
+
 echo "* Stopping service"
-sudo service KlipperScreen stop
+sudo systemctl stop KlipperScreen.service
+sudo systemctl disable KlipperScreen.service
+
 echo "* Removing unit file"
 sudo rm /etc/systemd/system/KlipperScreen.service
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+
 echo "* Removing environment"
 sudo rm -rf ~/.KlipperScreen-env
-echo "!! Please remove $(dirname `pwd`) manually"
+
+echo ""
+echo "* Uninstallation nearly complete. Please run:"
+echo "cd && rm -rf KlipperScreen"
+echo "to remove the source files"
+echo ""
 echo "Done"
