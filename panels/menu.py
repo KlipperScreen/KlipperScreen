ab5be917d975fd5a4ed7aea5b67fb6af8e59815a
J. P <q2@local>
2026-07-14 11:40:57 +0200

    Remove unused Template import

--- panels/menu.py
+++ panels/menu.py
@@ -5,7 +5,6 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-from jinja2 import Template
 
 from ks_includes.screen_panel import ScreenPanel
 from ks_includes.widgets.autogrid import AutoGrid
