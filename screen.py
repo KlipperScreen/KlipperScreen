209d02a366e9725a2efa0c5e26c17f25b3f24092
maddavo <1432875+maddavo@users.noreply.github.com>
2026-08-14 11:27:15 +1000

    Make success notifications non-modal

--- screen.py
+++ screen.py
@@ -505,6 +505,7 @@ class KlipperScreen(Gtk.ApplicationWindow):
             halign=Gtk.Align.CENTER,
             width_request=int(self.width * 0.9),
         )
+        popup.set_modal(level != 1)
         popup.get_style_context().add_class("message_popup_popover")
         popup.add(msg)
         popup.popup()
