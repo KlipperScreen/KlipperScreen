KlipperScreen/KlipperScreen

commit ff972bb75ff649b6d989b1a74c2affe48d1b7876

fix: only_heaters re-trying to add devices fixes #1762

panels/main_menu.py
@@ -16,6 +16,7 @@ class Panel(MenuPanel):
         super().__init__(screen, title, items)
         self.left_panel = None
         self.devices = {}
+        self._filtered = {}
         self.graph_update = None
         self.active_heater = None
         self.h = self.f = 0
@@ -118,6 +119,7 @@ class Panel(MenuPanel):
             class_name = f"graph_label_fan_{self.f}"
             dev_type = "fan"
         elif self._config.get_main_config().getboolean("only_heaters", False):
+            self._filtered[device] = True
             return False
         else:
             self.h += 1
@@ -159,6 +161,9 @@ class Panel(MenuPanel):
             "can_target": can_target,
             "visible": visible,
         }
+
+        if device not in self._filtered:
+            self._filtered[device] = True
 
         devices = sorted(self.devices)
         pos = devices.index(device) + 1
@@ -303,8 +308,10 @@ class Panel(MenuPanel):
         for device in self._printer.get_temp_devices():
             if device not in data:
                 continue
+            if device in self._filtered:
+                continue
             if device not in self.devices and not self.add_device(device):
-                return
+                continue
             self.update_temp(
                 device,
                 self._printer.get_stat(device, "temperature"),

panels/temperature.py
@@ -22,6 +22,7 @@ class Panel(ScreenPanel):
         super().__init__(screen, title)
         self.left_panel = None
         self.devices = {}
+        self._filtered = {}
         self.popover = Gtk.Popover(position=Gtk.PositionType.BOTTOM)
         self.popover_buttons = {}
         self.long_press = {}
@@ -349,6 +350,7 @@ class Panel(ScreenPanel):
             class_name = f"graph_label_fan_{self.f}"
             dev_type = "fan"
         elif self._config.get_main_config().getboolean("only_heaters", False):
+            self._filtered[device] = True
             return False
         else:
             self.h += 1
@@ -394,6 +396,9 @@ class Panel(ScreenPanel):
             "temp": temp,
             "visible": visible,
         }
+
+        if device not in self._filtered:
+            self._filtered[device] = True
 
         devices = sorted(self.devices)
         pos = devices.index(device) + 1
@@ -593,8 +598,10 @@ class Panel(ScreenPanel):
         for device in self._printer.get_temp_devices():
             if device not in data:
                 continue
+            if device in self._filtered:
+                continue
             if device not in self.devices and not self.add_device(device):
-                return
+                continue
             self.update_temp(
                 device,
                 self._printer.get_stat(device, "temperature"),

commit 10ded9b087aeccac6a69e2534cd4d4360d0118a9

fix: regression after ff972bb fixes #1769

panels/main_menu.py
@@ -162,9 +162,6 @@ class Panel(MenuPanel):
             "visible": visible,
         }
 
-        if device not in self._filtered:
-            self._filtered[device] = True
-
         devices = sorted(self.devices)
         pos = devices.index(device) + 1
 

panels/temperature.py
@@ -397,9 +397,6 @@ class Panel(ScreenPanel):
             "visible": visible,
         }
 
-        if device not in self._filtered:
-            self._filtered[device] = True
-
         devices = sorted(self.devices)
         pos = devices.index(device) + 1
 